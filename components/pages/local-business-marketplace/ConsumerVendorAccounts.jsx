a8e28d492f77d0dfb237c2b18e7a16e281c9d570
bgoette <bryantgoette@hotmail.com>
2022-02-14 10:41:30 -0600

    update hero banner for mobile

--- components/pages/local-business-marketplace/ConsumerVendorAccounts.jsx
+++ components/pages/local-business-marketplace/ConsumerVendorAccounts.jsx
@@ -7,7 +7,16 @@ export default function ConsumerVendorAccounts() {
             <Container fluid className="consumer-vendor-accounts">
                 <Container>
                     <Row>
-                        <Col lg={{ offset: 2, span: 8 }} className="block">
+                        <Col lg={6} className="block">
+                            <h2>
+                                Consumer Vendor Accounts
+                            </h2>
+                            <p>A consumer account is a term used in the credit reporting ndustry to describe items listed on a consumer&apos;s credit report. Each account displayed on a consumers credit report includes information about the creditor, history, balance and debt.</p>
+                            <p>
+                            Typically, credit repair companies will remove inaccurate or incomplete information from a consumer credit report, resulting in a skeletal credit profile. A skeletal credit profile is when you have a high credit score but no depth to show maturity. This results in denied credit applications. The B2C Partners network on the other hand, takes a different approach by rehabilitating a consumers credit profile with depth and a good credit mix. To highlight this distinction, the B2C partners network explains the benefits below:
+                            </p>
+                        </Col>
+                        <Col lg={6} className="block">
                             <h2>
                                 Consumer Vendor Accounts
                             </h2>
